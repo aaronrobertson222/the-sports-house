74bfef7986d5171fc18f6a8895836be706488dbc
aaronr5 <aaronr222@hotmail.com>
2019-05-15 00:32:24 -0400

    Updates

---
+++ precache-manifest.5a87e4605fd150d4268ab0a8124ea166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4b6e60a324eb835cdce22341c09f1b",
+    "url": "/the-sports-house/index.html"
+  },
+  {
+    "revision": "6f48ea22e1dfffced2b2",
+    "url": "/the-sports-house/static/css/main.c3d28bf5.chunk.css"
+  },
+  {
+    "revision": "7d4fa871868571f3d525",
+    "url": "/the-sports-house/static/js/2.8a144b7d.chunk.js"
+  },
+  {
+    "revision": "6f48ea22e1dfffced2b2",
+    "url": "/the-sports-house/static/js/main.68079158.chunk.js"
+  },
+  {
+    "revision": "861b5c7c3faabb48bd7a",
+    "url": "/the-sports-house/static/js/runtime~main.46e8b681.js"
+  },
+  {
+    "revision": "74eeb79cc69cc256ae282b02b641aa56",
+    "url": "/the-sports-house/static/media/main_logo.74eeb79c.png"
+  }
+]);
