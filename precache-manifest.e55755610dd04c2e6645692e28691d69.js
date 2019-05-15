513cc3bd3a6ba7be91fd2c1da07dd8db4acf74ae
aaronr5 <aaronr222@hotmail.com>
2019-05-14 23:33:16 -0400

    Updates

---
+++ precache-manifest.e55755610dd04c2e6645692e28691d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beea315f98a01d2aaad716f75db7a7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6419cd5035d32b8ba9d",
+    "url": "/static/css/main.c3d28bf5.chunk.css"
+  },
+  {
+    "revision": "bb1733878add563e7218",
+    "url": "/static/js/2.0bd4aed3.chunk.js"
+  },
+  {
+    "revision": "f6419cd5035d32b8ba9d",
+    "url": "/static/js/main.16483a71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74eeb79cc69cc256ae282b02b641aa56",
+    "url": "/static/media/main_logo.74eeb79c.png"
+  }
+]);
