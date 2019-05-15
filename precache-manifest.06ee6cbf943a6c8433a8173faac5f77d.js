79093eb512fd9b942260d93ce27073cfbde3bee4
aaronr5 <aaronr222@hotmail.com>
2019-05-14 23:50:55 -0400

    Updates

---
+++ precache-manifest.06ee6cbf943a6c8433a8173faac5f77d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3435c0d62f43d6862c6a4d7cd0d422f9",
+    "url": "/the-sports-house/index.html"
+  },
+  {
+    "revision": "ddbf2d65a6516af52077",
+    "url": "/the-sports-house/static/css/main.c3d28bf5.chunk.css"
+  },
+  {
+    "revision": "7d4fa871868571f3d525",
+    "url": "/the-sports-house/static/js/2.8a144b7d.chunk.js"
+  },
+  {
+    "revision": "ddbf2d65a6516af52077",
+    "url": "/the-sports-house/static/js/main.8469e2c0.chunk.js"
+  },
+  {
+    "revision": "861b5c7c3faabb48bd7a",
+    "url": "/the-sports-house/static/js/runtime~main.46e8b681.js"
+  },
+  {
+    "revision": "74eeb79cc69cc256ae282b02b641aa56",
+    "url": "/the-sports-house/static/media/main_logo.74eeb79c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e55755610dd04c2e6645692e28691d69.js"
+  "/the-sports-house/precache-manifest.06ee6cbf943a6c8433a8173faac5f77d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-sports-house/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
